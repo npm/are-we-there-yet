5794a498fd1acb6eb6da888b88c1f3ec8e929c71
Heri Sim <heri16@gmail.com>
2018-06-11 21:28:36 +0800

    Fix incorrect fallback pattern of readable-stream

--- tracker-stream.js
+++ tracker-stream.js
@@ -1,17 +1,17 @@
 'use strict'
 var util = require('util')
-var stream = require('readable-stream')
+var Transform = require('stream').Transform || require('readable-stream').Transform
 var delegate = require('delegates')
 var Tracker = require('./tracker.js')
 
 var TrackerStream = module.exports = function (name, size, options) {
-  stream.Transform.call(this, options)
+  Transform.call(this, options)
   this.tracker = new Tracker(name, size)
   this.name = name
   this.id = this.tracker.id
   this.tracker.on('change', delegateChange(this))
 }
-util.inherits(TrackerStream, stream.Transform)
+util.inherits(TrackerStream, Transform)
 
 function delegateChange (trackerStream) {
   return function (name, completion, tracker) {
